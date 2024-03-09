4fdaa87efca12e17ae68e67d82ac7b0411e0ec96
fawzanoachira <fawzanoachira@gmail.com>
2024-03-09 20:21:46 +0530

    file appending example

---
+++ F-O-L/FileHandling/src/FisFosDemo/AppendDemo.java
@@ -0,0 +1,7 @@
+package FisFosDemo;
+
+public class AppendDemo {
+    public static void main(String[] args) {
+        FileOutputStream fos = new FileOutputStream("filepath", true); //giving true specifying appending file will works. 
+    }
+}
