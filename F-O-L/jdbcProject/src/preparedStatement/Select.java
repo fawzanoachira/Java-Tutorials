3bc9d551c0530d97eee00025298430539e42fa34
fawzanicfoss <fawzanicfoss@gmail.com>
2024-05-24 17:03:11 +0530

    Select from prepared statement

--- F-O-L/jdbcProject/src/preparedStatement/Select.java
+++ F-O-L/jdbcProject/src/preparedStatement/Select.java
@@ -10,12 +10,15 @@ public class Select {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/notebook", "root", "icfoss007");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/notebook", "username", "password");
 
         PreparedStatement statement = connection.prepareStatement("select * from persondetails");
         ResultSet executeQuery = statement.executeQuery();
         while (executeQuery.next()) {
-            System.out.println(executeQuery.getInt(1) + " " + executeQuery.getString(2) + " " + executeQuery.getInt(3));
+            // System.out.println(executeQuery.getInt(1) + " " + executeQuery.getString(2) +
+            // " " + executeQuery.getInt(3));
+            System.out.println(executeQuery.getInt("personid") + " " + executeQuery.getString("personname") + " "
+                    + executeQuery.getInt("personage"));
         }
         connection.close();
     }
